9efc6721aef87cef27e318de90db94bcae3a3739
anaselgyar <160057586+anaselgyar@users.noreply.github.com>
2024-02-18 12:17:41 +0200

    caesar_brutforce.py

--- caesar_brutforce.py
+++ caesar_brutforce.py
@@ -1,17 +1,5 @@
-text = input('Enter your text:')
+ciphertext = input('Enter your text:')
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-key = int(input('Enter your cipher key:'))
-
-def encryption(text, key):
-    text = text.lower()
-    ciphertext = ""
-
-    for letter in text:
-        ciphertext += alphabet[(alphabet.index(letter) + key ) %26]
-
-    return ciphertext
-ciphertext = encryption(text, key)
-
 
 def decryption(ciphertext, key):
     ciphertext = ciphertext.lower()
@@ -21,8 +9,6 @@ def decryption(ciphertext, key):
         text += alphabet[(alphabet.index(letter) - key ) %26]
 
     return text
-decrypeted = decryption(ciphertext, key)
-
 
 def brutforce(ciphertext):
     for key in range(0, 26):
